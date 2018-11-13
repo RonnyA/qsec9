eccd206c1fd83aae7022e1b6e5b4a5d803409843
Ricardo Fiel <rifiel@microsoft.com>
2018-11-13 12:02:30 +0000

    Added swagger autogeneration

--- source/Controllers/SortingController.cs
+++ source/Controllers/SortingController.cs
@@ -11,5 +11,6 @@ namespace qsec9.Controllers
     [ApiController]
     public class SortingController : ControllerBase
     {
+
     }
 }
--- source/Startup.cs
+++ source/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace qsec9
 {
@@ -26,6 +27,12 @@ namespace qsec9
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "QSec 9 API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +46,16 @@ namespace qsec9
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "QSec 9 API v1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
